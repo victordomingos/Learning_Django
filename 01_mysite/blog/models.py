e78331b3cb67dde697ef86e8f0b22eca8f7fa9ba
victordomingos <editor.arcosonline@gmail.com>
2020-02-27 18:34:32 +0000

    model: artigo

--- 01_mysite/blog/models.py
+++ 01_mysite/blog/models.py
@@ -1,3 +1,18 @@
+from django.conf import settings
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Artigo(models.Model):
+    autor = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    titulo = models.CharField(max_length=200)
+    texto = models.TextField()
+    data_criacao = models.DateTimeField(default=timezone.now())
+    data_publicacao = models.DateTimeField(blank=True, null=True)
+
+    def publicar(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.titulo
