2d7c566c656573d498dad8f506366de8a561fb22
victordomingos <editor.arcosonline@gmail.com>
2020-02-28 22:51:10 +0000

    template: detalhe de artigo

--- 01_mysite/blog/urls.py
+++ 01_mysite/blog/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.lista_de_artigos, name='artigos')
+    path('', views.lista_de_artigos, name='artigos'),
+    path('artigo/<int:pk>/', views.artigo_detalhe, name='artigo_detalhe'),
 ]
--- 01_mysite/blog/views.py
+++ 01_mysite/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 
 from .models import Artigo
@@ -9,3 +9,8 @@ def lista_de_artigos(request):
         .filter(data_publicacao__lte=timezone.now()) \
         .order_by('data_publicacao')
     return render(request, 'blog/artigos.html', {'artigos': artigos})
+
+
+def artigo_detalhe(request, pk):
+    artigo = get_object_or_404(Artigo, pk=pk)
+    return render(request, 'blog/artigo_detalhe.html', {'artigo': artigo})
