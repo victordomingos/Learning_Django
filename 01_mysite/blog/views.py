847a2dcb72ad7ed95df1b9749b880476bace016e
victordomingos <editor.arcosonline@gmail.com>
2020-02-28 22:09:02 +0000

    template content block

--- 01_mysite/blog/views.py
+++ 01_mysite/blog/views.py
@@ -5,5 +5,7 @@ from .models import Artigo
 
 
 def lista_de_artigos(request):
-    artigos = Artigo.objects.filter(data_publicacao__lte=timezone.now()).order_by('data_publicacao')
+    artigos = Artigo.objects \
+        .filter(data_publicacao__lte=timezone.now()) \
+        .order_by('data_publicacao')
     return render(request, 'blog/artigos.html', {'artigos': artigos})
