460d68c7bac0ee9823ba51a2813e6524735b684a
victordomingos <editor.arcosonline@gmail.com>
2020-03-05 01:42:08 +0000

    models

--- 02_coolsite/coolsite/settings.py
+++ 02_coolsite/coolsite/settings.py
@@ -103,9 +103,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-pt'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Lisbon'
 
 USE_I18N = True
 
--- 02_coolsite/sondagens/models.py
+++ 02_coolsite/sondagens/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Questao(models.Model):
+    texto = models.CharField(max_length=200)
+    data_publicacao = models.DateTimeField('date_published')
+
+
+class Escolha(models.Model):
+    questao = models.ForeignKey(Questao, on_delete=models.CASCADE)
+    texto = models.CharField(max_length=200)
+    votos = models.IntegerField(default=0)
